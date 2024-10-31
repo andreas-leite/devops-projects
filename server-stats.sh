bd2e67205872e03b10aae9e65618e5e94a6c0c78
Andreas Leite <leite.andreas@gmail.com>
2024-10-31 18:21:46 +0100

    added CPU usage to server-stats.sh

---
+++ server-stats.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Get total CPU Usage
+CPU_USAGE=$(top -bn1 | grep "Cpu(s)" | sed "s/.*, *\([0-9.]*\)%* id.*/\1/" | awk '{print 100 - $1"%"}')
+
+# Display CPU usage
+echo "Total CPU Usage: $CPU_USAGE"
